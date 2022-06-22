ae835bac03a080380c22560b77f0d7c6da3e5ae5
gabriel agregatti alves <106711879+EOAGREGATTI@users.noreply.github.com>
2022-06-22 20:44:37 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+let xbolinha = 300;
+let ybolinha = 200;
+let diametro = 15;
+
+let velocidadexbolinha = 6;
+let velocidadeybolinha = 6;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  circle(xbolinha,ybolinha,diametro);
+  xbolinha += velocidadexbolinha;
+  //ybolinha += velocidadeybolinha;
+  
+  if (xbolinha > width ou
+     xbolinha <){
+    velocidadexbolinha *= -1;
+  }
+}
